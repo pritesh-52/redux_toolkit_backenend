d02e9cac42be9215e3f721235b1cee1aa07a29b4
pritesh-52 <priteshbhatiya52@gmail.com>
2024-01-25 17:09:20 +0530

    modification

--- src/model/studentmodel.js
+++ src/model/studentmodel.js
@@ -1,12 +1,9 @@
 const Pool = require("pg").Pool;
 require("dotenv").config();
-
 const pool = new Pool({
-  user: "my_user",
-  database: "student",
-  port: 5432,
-  password: "root",
-  host: "localhost",
-});
+
+    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+  
+  })
 
 module.exports = pool;
