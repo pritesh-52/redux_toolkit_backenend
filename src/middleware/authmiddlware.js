86c9cadf858abba47e3fbb1748a670621198d486
pritesh-52 <priteshbhatiya52@gmail.com>
2024-01-28 16:35:20 +0530

    Add auth middleware

--- src/middleware/authmiddlware.js
+++ src/middleware/authmiddlware.js
@@ -3,18 +3,29 @@ require("dotenv").config();
 const SECRET_KEY = process.env.SECRET_KEY;
 
 const auth = async (req, res, next) => {
+  const token = req.headers.authorization;
+  console.log(token);
+  if (!token) {
+    return res.status(401).json({ message: 'No token, authorization denied' });
+  }
   try {
-    let token = req.headers.authorization;
-    if (token) {
-      token = token.split(" ")[1];
-      let user = jwt.verify(token, SECRET_KEY);
-      req.id = user.id;
-    } else {
-      res.status(400).json({ message: "Unauthories user" });
-    }
+    const data = jwt.verify(token, SECRET_KEY);
+    req.user = data;
+    console.log(data);
     next();
   } catch (e) {
-    res.status(405).json({ message: "Something went wrong" });
+    if(e.name==="TokenExpiredError")
+    {
+      return res.status(401).json({ message: 'Token has expired' });
+    }
+    else if (e.name === 'JsonWebTokenError') {
+      return res.status(401).json({ message: 'Invalid token format or signature' });
+    }
+
+
+    console.error('Error during token verification:', e.message);
+    res.status(401).json({ message: 'Token is not valid' });
+    
   }
 };
 
--- src/router/router.js
+++ src/router/router.js
@@ -4,10 +4,9 @@ const controller = require("../controller/studentcontroller");
 const logincontroller=require("../controller/logincontroller");
 const auth=require("../middleware/authmiddlware");
 
-router.get("/getstudents", controller.getstudent);
+router.get("/getstudents",auth,controller.getstudent);
 router.post("/addstudent", controller.addstudent);
 router.put("/updatestudent/:id", controller.updatestudent);
 router.delete("/deletestudent/:id", controller.deletestudent);
 router.post("/getlogindata",logincontroller.loginuser);
-
 module.exports = router;
