a0b45bd8e7c91fcb844fa8b3cdf2ed400b2f1eb8
pritesh-52 <priteshbhatiya52@gmail.com>
2024-01-29 11:55:13 +0530

    Update files

--- src/controller/logincontroller.js
+++ src/controller/logincontroller.js
@@ -1,9 +1,16 @@
 const pool = require("../model/studentmodel");
 const jwt = require("jsonwebtoken");
+const {validationResult} = require("express-validator");
+const { json } = require("express");
 require("dotenv").config();
 const SECRET_KEY = process.env.SECRET_KEY;
 
 exports.loginuser = async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
   try {
     const { uname, password } = req.body;
 
@@ -15,14 +22,11 @@ exports.loginuser = async (req, res) => {
       const authtoken = jwt.sign(
         { uname: user.rows[0].uname, id: user.rows[0].id },
         SECRET_KEY
-      )
-      res.status(200).send({ message: "User Logged In Success"});
-      //res.json({authtoken,success:true});
+      );
+      res.status(200).send({ message: "User Logged In Success" });
       console.log(authtoken);
-      
     } else {
-      return res.status(400).json({ message: "Invalid username or password"});
-      
+      return res.json({ errors: "Inavaild Emaild Or Password" });
     }
   } catch (e) {
     console.error(e);
